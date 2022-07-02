fe9227eab5f2c5c182099842efbaa8438dcfc052
narekpetrosyan <narekpetrosyan199823@gmail.com>
2022-07-02 21:06:14 +0400

    initialized app

---
+++ src/auth.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request
+from werkzeug.security import check_password_hash, generate_password_hash
+
+auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
+
+@auth.post("/register")
+def register():
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+    
+    return {"user": "created"}
+
+@auth.get("/me")
+def me():
+    return {"user": "created"}
